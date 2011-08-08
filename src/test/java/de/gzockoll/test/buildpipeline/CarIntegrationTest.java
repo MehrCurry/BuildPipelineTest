ef06d01fde8164842115fe55bda0c6ab24d6327a
Guido Zockoll <gzockoll@gmail.com>
2011-08-08 14:20:08 +0200

    Integrations test runs

--- src/test/java/de/gzockoll/test/buildpipeline/CarIntegrationTest.java
+++ src/test/java/de/gzockoll/test/buildpipeline/CarIntegrationTest.java
@@ -1,13 +1,11 @@
 package de.gzockoll.test.buildpipeline;
 
-import org.junit.Test;
 
 
 public class CarIntegrationTest extends SeleniumTestCase {
 	
-	// @Test
 	public void testAddCar() throws Exception {
-		selenium.open("/BuildPipelineTest/hello/");
+		selenium.open("/BuildPipelineTest/hello");
 		selenium.waitForPageToLoad("5000");
 		assertTrue(selenium.isTextPresent("Hello Servlet"));
 	}	
